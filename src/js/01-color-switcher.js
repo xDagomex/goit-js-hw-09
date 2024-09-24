a0e4c5196195839ef98807d616d5c144a5415ffa
xDagomex <adaskubiak@wp.pl>
2024-09-24 02:43:04 +0200

    4

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,3 +4,6 @@ function getRandomHexColor() {  
     
 const startBtn = document.querySelector('[data-start');
 const stopBtn = document.querySelector('[data-stop');
+
+console.log(startBtn);
+console.log(stopBtn);
