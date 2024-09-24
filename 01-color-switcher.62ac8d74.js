b15dde5f3717ca2cdc818e97308841f1655b6b55
xDagomex <161179024+xDagomex@users.noreply.github.com>
2024-09-24 01:01:11 +0000

    Deploying to gh-pages from @ xDagomex/goit-js-hw-09@4c53c047b12eaab709ad83480e7a30812043285f 🚀

---
+++ 01-color-switcher.62ac8d74.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start"),e=document.querySelector("[data-stop"),n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.62ac8d74.js.map
--- 01-color-switcher.f8094ee8.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start"),e=document.querySelector("[data-stop"),n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),10)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.f8094ee8.js.map
