7a59190b9ced9107f0fe18a12b9fc666d4a1ee7a
xDagomex <161179024+xDagomex@users.noreply.github.com>
2024-09-24 00:43:25 +0000

    Deploying to gh-pages from @ xDagomex/goit-js-hw-09@a0e4c5196195839ef98807d616d5c144a5415ffa 🚀

--- 01-color-switcher.4c380208.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("[data-start"),document.querySelector("[data-stop");
-//# sourceMappingURL=01-color-switcher.4c380208.js.map
---
+++ 01-color-switcher.c7df676d.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("[data-start"),e=document.querySelector("[data-stop");console.log(o),console.log(e)}();
+//# sourceMappingURL=01-color-switcher.c7df676d.js.map
